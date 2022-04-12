3e0ba60e2b634ef74f8ac5639673416ab9030f40
thijsBoet <m.boet@chello.nl>
2022-04-12 15:07:24 +0200

    setting up middleware wont logout Sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -5,53 +5,64 @@ import {
 	PlusCircleIcon,
 	HeartIcon,
 	RssIcon,
+	LogoutIcon,
 } from '@heroicons/react/outline';
+import { signOut, useSession } from 'next-auth/react';
 
 const Sidebar = () => {
+	const { data: session, status } = useSession();
+	
 	return (
-		<div className='text-gray-500 p-5 text-sm border-r border-gray-900 space-y-4'>
+		<div className='p-5 space-y-4 text-sm text-gray-500 border-r border-gray-900'>
+			<button
+				className='flex items-center space-x-2 hover:text-white '
+				onClick={() => signOut()}
+			>
+				<LogoutIcon className='w-5 h-w' />
+				<p>Logout</p>
+			</button>
 			<button className='flex items-center space-x-2 hover:text-white '>
-				<HomeIcon className='h-w w-5' />
+				<HomeIcon className='w-5 h-w' />
 				<p>Home</p>
 			</button>
 			<button className='flex items-center space-x-2 hover:text-white '>
-				<SearchIcon className='h-w w-5' />
+				<SearchIcon className='w-5 h-w' />
 				<p>Search</p>
 			</button>
 			<button className='flex items-center space-x-2 hover:text-white '>
-				<LibraryIcon className='h-w w-5' />
+				<LibraryIcon className='w-5 h-w' />
 				<p>Your Library</p>
 			</button>
 			<hr className='border-t-[0.1px] border-gray-900' />
 			<button className='flex items-center space-x-2 hover:text-white '>
-				<PlusCircleIcon className='h-w w-5' />
+				<PlusCircleIcon className='w-5 h-w' />
 				<p>Create Playlist</p>
 			</button>
 			<button className='flex items-center space-x-2 hover:text-white '>
-				<HeartIcon className='h-w w-5' />
+				<HeartIcon className='w-5 h-w' />
 				<p>Likes Songs</p>
 			</button>
 			<button className='flex items-center space-x-2 hover:text-white '>
-				<RssIcon className='h-w w-5' />
+				<RssIcon className='w-5 h-w' />
 				<p>Your Episodes</p>
 			</button>
 			<hr className='border-t-[0.1px] border-gray-900' />
-			
+
 			{/* Playlist  */}
-			<p className="cursor-pointer hover:text-white">Playlist name...</p>
-			<p className="cursor-pointer hover:text-white">Playlist name...</p>
-			<p className="cursor-pointer hover:text-white">Playlist name...</p>
-			<p className="cursor-pointer hover:text-white">Playlist name...</p>
-			<p className="cursor-pointer hover:text-white">Playlist name...</p>
-			<p className="cursor-pointer hover:text-white">Playlist name...</p>
-			<p className="cursor-pointer hover:text-white">Playlist name...</p>
-			<p className="cursor-pointer hover:text-white">Playlist name...</p>
-			<p className="cursor-pointer hover:text-white">Playlist name...</p>
-			<p className="cursor-pointer hover:text-white">Playlist name...</p>
-			<p className="cursor-pointer hover:text-white">Playlist name...</p>
-			<p className="cursor-pointer hover:text-white">Playlist name...</p>
+			<p className='cursor-pointer hover:text-white'>Playlist name...</p>
+			<p className='cursor-pointer hover:text-white'>Playlist name...</p>
+			<p className='cursor-pointer hover:text-white'>Playlist name...</p>
+			<p className='cursor-pointer hover:text-white'>Playlist name...</p>
+			<p className='cursor-pointer hover:text-white'>Playlist name...</p>
+			<p className='cursor-pointer hover:text-white'>Playlist name...</p>
+			<p className='cursor-pointer hover:text-white'>Playlist name...</p>
+			<p className='cursor-pointer hover:text-white'>Playlist name...</p>
+			<p className='cursor-pointer hover:text-white'>Playlist name...</p>
+			<p className='cursor-pointer hover:text-white'>Playlist name...</p>
+			<p className='cursor-pointer hover:text-white'>Playlist name...</p>
+			<p className='cursor-pointer hover:text-white'>Playlist name...</p>
 		</div>
 	);
-}
+};
 
 export default Sidebar;
--- pages/login.js
+++ pages/login.js
@@ -1,32 +1,30 @@
-import { getProviders, SignIn } from 'next-auth/react';
-import Image from 'next/image';
+import { getProviders, signIn } from 'next-auth/react';
+
 
 const Login = ({ providers }) => {
 	return (
 		<div className='flex flex-col items-center justify-center w-full min-h-screen bg-black' >
-			<Image
-				width={768}
-				height={768}
+			{/* <Image
+				width={208}
+				height={208}
 				src='https://links.papareact.com/9xl'
 				alt='Spotify logo'
 				className='mb-5 w-52'
-			/>
-			{Object.values(providers).map((provider) => {
+			/> */}
+			<img src="https://links.papareact.com/9xl" alt='Spotify logo' className='mb-5 w-52'/>
+			{Object.values(providers).map((provider) => (
 				<div key={provider.name}>
 					<button
-						className='bg-[#18D860] text-white p-5 rounded-full'
-						onClick={() => { SignIn(provider.id, {callbackUrl: '/'}) }}
+						className='bg-[#18D860] text-white p-5 rounded-full cursor-pointer'
+						onClick={() =>  signIn(provider.id, {callbackUrl: '/'})}
 					>
 						Login with {provider.name}
-						{console.log(provider)}
 					</button>
-				</div>;
-			})}
+				</div>
+			))}
 		</div>
 	);
 };
-
-export default Login;
 
 export const getServerSideProps = async () => {
 	const providers = await getProviders();
@@ -37,3 +35,5 @@ export const getServerSideProps = async () => {
 		},
 	};
 };
+
+export default Login;
